d766c9e5b5661adf80392baf649c0d5691409f91
Gerardo Guevara <gerardoguevara64@gmail.com>
2020-12-01 10:46:35 -0600

    setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { NgbootstrapModule } from './ngb/ngbootstrap/ngbootstrap.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// ng bootstrap
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ngb/ngbootstrap/ngbootstrap.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+const components = [];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: []
+})
+export class NgbootstrapModule { }
